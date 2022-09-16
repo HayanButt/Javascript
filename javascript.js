952bc77465d704916cebffb2d0a8cb14053945aa
Hayan Butt <hayan_880@hotmail.com>
2022-09-16 19:23:24 +0100

    Starting to learn javascript - focus on variables

---
+++ javascript.js
@@ -0,0 +1,43 @@
+//Example of a comment
+/*Example of
+    a multiline comment */
+
+//Example of storing variables with each method
+
+var myName = "Hayan";
+//Changing contents of myName
+myName = "Hayyyyaaaan";
+let lastName = "Butt";
+const number = "42";
+//outputting variables to the console
+console.log(myName);
+console.log(lastName);
+console.log(number);
+
+//difference between declaring and assigning a variable
+//declaring variable 'a':
+var a;
+//outputting variable a to the console
+console.log(a);
+//assigning:
+var b = 2;
+//recalling variable 'a' and assigning data to it
+a = 7;
+//outputting variable a to the console
+console.log(a);
+
+//Maths functions
+//Addition
+var myVar1 = 1 + 1;
+//Substraction
+var myVar2 = 2 - 1;
+//Multiplication
+var myVar3 = 2 * 2;
+//Division
+var myVar4 = 10 / 5;
+//Incrementation
+var myVar5 = 34;
+myVar5++
+//Decrementation
+var myVar6 = 34;
+myVar6--
